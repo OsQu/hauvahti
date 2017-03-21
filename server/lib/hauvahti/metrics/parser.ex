698aaaa76571a904037c748b2aca651e777a2f56
Oskari Virtanen <oskari@flowdock.com>
2017-03-21 20:33:23 +0200

    Implement event parser

---
+++ server/lib/hauvahti/metrics/parser.ex
@@ -0,0 +1,29 @@
+defmodule Hauvahti.Metrics.Parser do
+  use GenServer
+
+  def start_link(name, event_channel) do
+    GenServer.start_link(__MODULE__, event_channel, name: name)
+  end
+
+  def parse(server, user, events) do
+    GenServer.cast(server, {:parse_event, user, events})
+  end
+
+  def handle_cast({:parse_event, user, events}, event_channel) do
+    Enum.each(parse_events(events), fn ([key, value]) ->
+      GenEvent.notify(event_channel, {:incoming_event, user, %{key => value}})
+    end)
+    {:noreply, event_channel}
+  end
+
+  defp parse_events(events) when is_bitstring(events) do
+    parse_events(String.split(events, ","))
+  end
+
+  defp parse_events(events) when is_list(events) do
+    Enum.map(events, fn (event) ->
+      [key, value] = String.split(event, "=")
+      [key, String.to_integer(value)]
+    end)
+  end
+end
---
+++ server/test/lib/hauvahti/metrics/parser_test.exs
@@ -0,0 +1,44 @@
+defmodule Hauvahti.Metrics.ParserTest do
+  use ExUnit.Case, async: true
+
+  alias Hauvahti.Metrics.Parser
+
+  defmodule Listener do
+    use GenEvent
+
+    def handle_event(msg, test_pid) do
+      send(test_pid, msg)
+      {:ok, test_pid}
+    end
+  end
+
+  setup context do
+    {:ok, channel} = GenEvent.start_link()
+    GenEvent.add_handler(channel, Listener, self())
+
+    {:ok, parser} = Parser.start_link(context.test, channel)
+    {:ok, parser: parser}
+  end
+
+  test "notifying Hauvahti.Metrics.Events with parsed events", %{parser: parser} do
+    Parser.parse(parser, 1, "volume=10")
+    assert_receive {:incoming_event, 1, %{"volume" => 10}}
+  end
+
+  test "parsing multiple events from same user", %{parser: parser} do
+    Parser.parse(parser, 1, "volume=10,volume=20")
+    assert_receive {:incoming_event, 1, %{"volume" => 10}}
+    assert_receive {:incoming_event, 1, %{"volume" => 20}}
+  end
+
+  test "parsing multiple events from many users", %{parser: parser} do
+    Parser.parse(parser, 1, "volume=10,volume=20")
+    Parser.parse(parser, 2, "volume=30")
+    Parser.parse(parser, 1, "volume=40")
+
+    assert_receive {:incoming_event, 1, %{"volume" => 10}}
+    assert_receive {:incoming_event, 1, %{"volume" => 20}}
+    assert_receive {:incoming_event, 1, %{"volume" => 40}}
+    assert_receive {:incoming_event, 2, %{"volume" => 30}}
+  end
+end
